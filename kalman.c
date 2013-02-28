089b2d92479edbf5b9e2c6dbd236185c5ef8ac73
Joel Matthys <jwmatthys@yahoo.com>
2013-02-28 12:31:12 -0500

    create object methods and outlet

--- kalman.c
+++ kalman.c
@@ -1,49 +1,65 @@
-/* code for "mycobject" pd class.  This takes two messages: floating-point
-numbers, and "rats", and just prints something out for each message. */
+/* code for "kalman" pd class.  This takes two messages: floating-point
+   numbers, and "rats", and just prints something out for each message. */
 
 #include "m_pd.h"
 
-    /* the data structure for each copy of "mycobject".  In this case we
-    on;y need pd's obligatory header (of type t_object). */
-typedef struct mycobject
+/* the data structure for each copy of "kalman".  In this case we
+   only need pd's obligatory header (of type t_object). */
+typedef struct kalman
 {
   t_object x_ob;
-} t_mycobject;
+} t_kalman;
 
-    /* this is called back when mycobject gets a "float" message (i.e., a
-    number.) */
-void mycobject_float(t_mycobject *x, t_floatarg f)
+/* this is called back when kalman gets a "float" message (i.e., a
+   number.) */
+void kalman_float(t_kalman *x, t_floatarg f)
 {
-    post("mycobject: %f", f);
-    x=NULL; /* don't warn about unused variables */
+  post("kalman: %f", f);
+  outlet_float (x->x_ob.ob_outlet, f);
+  x=NULL; /* don't warn about unused variables */
 }
 
-    /* this is called when mycobject gets the message, "rats". */
-void mycobject_rats(t_mycobject *x)
+/* this is called when kalman gets the message, "noise". */
+void kalman_setnoise(t_kalman *x, t_float f)
 {
-    post("mycobject: rats");
-    x=NULL; /* don't warn about unused variables */
+  post("kalman: noise set to %f", f);
+  x=NULL; /* don't warn about unused variables */
 }
 
-    /* this is a pointer to the class for "mycobject", which is created in the
-    "setup" routine below and used to create new ones in the "new" routine. */
-t_class *mycobject_class;
-
-    /* this is called when a new "mycobject" object is created. */
-void *mycobject_new(void)
+/* this is called when kalman gets the message, "init". */
+void kalman_setinit(t_kalman *x, t_float f)
 {
-    t_mycobject *x = (t_mycobject *)pd_new(mycobject_class);
-    post("mycobject_new");
-    return (void *)x;
+  post("kalman: init val set to %f", f);
+  x=NULL; /* don't warn about unused variables */
 }
 
-    /* this is called once at setup time, when this code is loaded into Pd. */
-void mycobject_setup(void)
+
+/* this is a pointer to the class for "kalman", which is created in the
+   "setup" routine below and used to create new ones in the "new" routine. */
+t_class *kalman_class;
+
+/* this is called when a new "kalman" object is created. */
+void *kalman_new(t_symbol *s, int argc, t_atom *argv)
 {
-    post("mycobject_setup");
-    mycobject_class = class_new(gensym("mycobject"), (t_newmethod)mycobject_new, 0,
-    	sizeof(t_mycobject), 0, 0);
-    class_addmethod(mycobject_class, (t_method)mycobject_rats, gensym("rats"), 0);
-    class_addfloat(mycobject_class, mycobject_float);
+  t_kalman *x = (t_kalman *)pd_new(kalman_class);
+  outlet_new(&x->x_ob, &s_float);
+  post("kalman_new");
+  // initialize variables here
+  return (void *)x;
 }
 
+void kalman_free(t_kalman *x)
+{
+  // free array memory if malloc'ed
+}
+
+/* this is called once at setup time, when this code is loaded into Pd. */
+void kalman_setup(void)
+{
+  post("kalman_setup");
+  kalman_class = class_new(gensym("kalman"), (t_newmethod)kalman_new, kalman_free,
+                           sizeof(t_kalman), 0, A_GIMME, 0);
+  class_addmethod(kalman_class, (t_method)kalman_setnoise, gensym("noise"), A_FLOAT, 0);
+  class_addmethod(kalman_class, (t_method)kalman_setinit, gensym("init"), A_FLOAT, 0);
+  class_addfloat(kalman_class, kalman_float);
+}
